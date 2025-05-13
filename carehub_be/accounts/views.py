d6406915e45d4f494d56623f96cb15efa8027985
Yusrâ Arigui <ariguiyusra@gmail.com>
2025-05-13 16:07:50 +0200

    adding postgre

--- carehub_be/accounts/views.py
+++ carehub_be/accounts/views.py
@@ -11,6 +11,14 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
 
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        user = User.objects.get(email=request.data['email'])
+        refresh = RefreshToken.for_user(user)
+        response.data['refresh'] = str(refresh)
+        response.data['access'] = str(refresh.access_token)
+        return response
+
 class ProfileView(APIView):
     permission_classes = [IsAuthenticated]
 
--- carehub_be/carehub_be/settings.py
+++ carehub_be/carehub_be/settings.py
@@ -95,8 +95,12 @@ WSGI_APPLICATION = 'carehub_be.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carehub_db',
+        'USER': 'carehub_user',
+        'PASSWORD': '28469',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
